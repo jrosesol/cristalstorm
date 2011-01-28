f5143234237df54765298b358302369b7e4e70ed
Jose Rose <jrosesol@gmail.com>
2011-01-27 21:02:34 -0500

    + For now I added gwt-dnd AND gwtquery-pluggin dependency. Probably will only keep gwtquery. + Very small proto snippet to prove the dnd concept. + Temporarily removed CSS templates (they are commmented in soft.gwt.xml and soft.html).

--- src/main/java/com/cristal/storm/prototype/client/mvp/view/AppStartPageView.java
+++ src/main/java/com/cristal/storm/prototype/client/mvp/view/AppStartPageView.java
@@ -6,13 +6,37 @@
  */
 package com.cristal.storm.prototype.client.mvp.view;
 
+import gwtquery.plugins.draggable.client.DraggableOptions;
+import gwtquery.plugins.draggable.client.DraggableOptions.HelperType;
+import gwtquery.plugins.droppable.client.DroppableOptions.DroppableTolerance;
+import gwtquery.plugins.droppable.client.events.DropEvent;
+import gwtquery.plugins.droppable.client.events.DropEvent.DropEventHandler;
+import gwtquery.plugins.droppable.client.gwt.DragAndDropCellList;
+import gwtquery.plugins.droppable.client.gwt.DroppableWidget;
+
+import java.util.Arrays;
+import java.util.List;
+
+import com.google.gwt.cell.client.AbstractCell;
+import com.google.gwt.cell.client.Cell;
+import com.google.gwt.cell.client.TextCell;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.safehtml.shared.SafeHtmlBuilder;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.uibinder.client.UiHandler;
+import com.google.gwt.user.cellview.client.CellList;
+import com.google.gwt.user.cellview.client.CellList.Resources;
+import com.google.gwt.user.cellview.client.HasKeyboardSelectionPolicy.KeyboardSelectionPolicy;
+import com.google.gwt.user.client.ui.AbsolutePanel;
 import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.FlowPanel;
+import com.google.gwt.user.client.ui.HorizontalPanel;
+import com.google.gwt.user.client.ui.Image;
+import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.ListBox;
+import com.google.gwt.user.client.ui.RootPanel;
 import com.google.gwt.user.client.ui.SimplePanel;
 import com.google.gwt.user.client.ui.TextBox;
 import com.google.gwt.user.client.ui.VerticalPanel;
@@ -56,8 +80,14 @@ public class AppStartPageView extends ViewWithUiHandlers<AppStartPageUiHandlers>
     
     @UiField
     ListBox mcnStack;
+    
+    @UiField
+    AbsolutePanel centerAbsPanel;
 
     private final Widget widget;
+    
+    private static final List<String> DAYS = Arrays.asList("Sunday", "Monday",
+            "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday");
 
     ///////////////////////////////////////////////////////////////////////////
     // Interfaces
@@ -65,6 +95,29 @@ public class AppStartPageView extends ViewWithUiHandlers<AppStartPageUiHandlers>
 
     interface AppStartPageViewUiBinder extends UiBinder<Widget, AppStartPageView> {
     }
+    
+    private static class MyTextCell extends AbstractCell<String> {
+
+        public MyTextCell() {
+        }
+
+        @Override
+        public void render(String value, Object key, SafeHtmlBuilder sb) {
+          // Value can be null, so do a null check..
+          if (value == null) {
+            return;
+          }
+
+          sb.appendHtmlConstant("<table>");
+
+          // Add the name and address.
+          sb.appendHtmlConstant("<td style='font-size:95%;'>");
+          sb.appendEscaped(value);
+          sb.appendHtmlConstant("</td></tr></table>");
+        }
+      }
+    
+    private static Resources DEFAULT_RESOURCES = GWT.create(Resources.class);
 
     ///////////////////////////////////////////////////////////////////////////
     // Constructors
@@ -75,6 +128,91 @@ public class AppStartPageView extends ViewWithUiHandlers<AppStartPageUiHandlers>
 
         mainLayout.add(commandLineBox.asWidget());
         simplePanel.add(topView.asWidget());
+        
+        // create a DragController to manage drag-n-drop actions
+        // note: This creates an implicit DropController for the boundary panel
+        //PickupDragController dragController = new PickupDragController(centerAbsPanel, true);
+
+        HorizontalPanel horzPanel = new HorizontalPanel();
+        
+        // Create a cell to render each value.
+        //MyTextCell textCell = new MyTextCell();
+        TextCell textCell = new TextCell();
+        
+        
+        //CellList<String> cellList = new CellList<String>(textCell);
+
+        DragAndDropCellList<String> dndCellList = new DragAndDropCellList<String>(textCell, DEFAULT_RESOURCES);
+
+
+        dndCellList.setKeyboardSelectionPolicy(KeyboardSelectionPolicy.ENABLED);
+        // Push the data into the widget.
+        dndCellList.setRowData(0, DAYS);
+        
+        
+        // The cell of this CellList are only draggable
+        dndCellList.setCellDraggableOnly();
+        
+        // setup the drag operation
+        DraggableOptions options = new DraggableOptions();
+        // use a clone of the original cell as drag helper
+        options.setHelper(HelperType.CLONE);
+        // set the opacity of the drag helper
+        options.setOpacity((float) 0.9);
+        // append the drag helper to the body element
+        options.setAppendTo("body");
+        // configure the drag operations of the cell list with this options
+        dndCellList.setDraggableOptions(options);
+        
+        horzPanel.add(dndCellList);
+        
+        /**
+         * Create a droppable CellList
+         */
+        final FlowPanel aGwtPanel = new FlowPanel();
+        aGwtPanel.add(new Label("test"));
+        DroppableWidget<FlowPanel> droppablePanel = new DroppableWidget<FlowPanel>(aGwtPanel);
+        // configure the drop behaviour (see next paragraph)
+        droppablePanel.setTolerance(DroppableTolerance.POINTER);
+        
+        droppablePanel.addDropHandler(new DropEventHandler() {
+
+            public void onDrop(DropEvent event) {
+                // retrieve the droppable widget
+                // DroppableWidget<Label> droppableLabel =
+                // (DroppableWidget<Label>)event.getDroppableWidget();
+                // retrieve the dropped draggable widget (we assume it is a
+                // draggable label)
+                // DraggableWidget<Label> draggableLabel =
+                // (DraggableWidget<Label>)event.getDraggableWidget();
+
+                // droppableLabel.getOriginalWidget().setText("I ate : "+draggableLabel.getOriginalWidget().getText());
+
+                // remove the draggabeLable
+                // draggableLabel.removeFromParent();
+                aGwtPanel.add(new Label("just dragged item"));
+                aGwtPanel.add(new Label("just dragged item"));
+                aGwtPanel.add(new Label("just dragged item"));
+                aGwtPanel.add(new Label("just dragged item"));
+                aGwtPanel.add(new Label("just dragged item"));
+                aGwtPanel.add(new Label("just dragged item"));
+                aGwtPanel.add(new Label("just dragged item"));
+                aGwtPanel.add(new Label("just dragged item"));
+                aGwtPanel.add(new Label("just dragged item"));
+                aGwtPanel.add(new Label("just dragged item"));
+                aGwtPanel.add(new Label("just dragged item"));
+                aGwtPanel.add(new Label("just dragged item"));
+                aGwtPanel.add(new Label("just dragged item"));
+                aGwtPanel.add(new Label("just dragged item"));
+            }
+        });
+        
+        
+        horzPanel.add(droppablePanel);
+        
+        centerAbsPanel.add(horzPanel);
+
+        //dragController.makeDraggable(horzPanel);
     }
 
     ///////////////////////////////////////////////////////////////////////////
